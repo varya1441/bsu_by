7d7e58d36fe497cb55ffeb0445144e599ae17cb5
Varvara <varya1441@gmail.com>
2018-11-18 11:22:54 +0300

    added 10

--- lab10/src/com/tihonova/lab10/runner/Runner.java
+++ lab10/src/com/tihonova/lab10/runner/Runner.java
@@ -2,8 +2,6 @@ package com.tihonova.lab10.runner;
 
 import com.tihonova.lab10.domain.MyWindow;
 
-import javax.swing.*;
-
 public class Runner {
     public static void main(String[] args) {
         MyWindow mywindow=new MyWindow();
